49fdcbf40bf7a6ace9bfd9f21a52624af417330a
ihsan dihkan <dihkan@gmail.com>
2024-01-21 14:24:25 +0300

    gitignore eklendi

--- app2/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userRouter from './users.js';
-const app = express();
-
-app.use('/users' , userRouter);
-
-app.listen(3000 , ()=>{
-    console.log("Sunucu http://localhost:3000 adresinde çalışıyor");
-})
